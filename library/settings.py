05154514fec7ca6acbfe0c2476196ae1c332a4bd
Deaath65 <deaath65@gmail.com>
2023-11-30 14:52:46 +0545

    custom user

--- library/settings.py
+++ library/settings.py
@@ -26,7 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
