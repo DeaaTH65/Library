f93945c6e72dffedca3d33681e8c052e4564c96c
Deaath65 <deaath65@gmail.com>
2023-12-12 10:46:18 +0545

    Reservation model

--- main/models.py
+++ main/models.py
@@ -12,4 +12,16 @@ class Book(models.Model):
     
     def __str__(self):
         return self.name
-    
+
+
+class Reservation(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    reserved_by = models.CharField(max_length=250)
+    reservation_date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.book.name} - Reserved by {self.reserved_by}"
+
+    class Meta:
+        ordering = ['-reservation_date']
