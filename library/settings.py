11f03146322a7588728affab1f8327f80d5c67b2
Deaath65 <deaath65@gmail.com>
2023-12-28 15:19:56 +0545

    Newsletter

--- library/settings.py
+++ library/settings.py
@@ -79,6 +79,14 @@ DATABASES = {
 }
 
 
+# Emailing settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'deaath65@gmail.com'
+EMAIL_HOST_USER = 'deaath65@gmail.com'
+EMAIL_HOST_PASSWORD = 'motz mzfs dcji snmn'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
 # Password validation
